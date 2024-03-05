009d29e482f58cb0af2227a86263e93726749b47
Karine <karine.fbueno@gmail.com>
2024-03-05 18:15:27 -0300

    C01-Configurações projeto

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { GlobalStyle } from './styles/global-styles';
+
+function App() {
+  return (
+    <GlobalStyle />
+  );
+}
+
+export default App;
---
+++ src/styles/global-styles.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Nunito';
+    font-style: normal;
+    font-weight: 200;
+    src: url('https://fonts.gstatic.com/s/nunito/v16/XRXV3I6Li01BKofINeaBTMnFcQIG.ttf') format('truetype');
+  }
+
+  :root {
+    --font-family: 'Nunito', sans-serif;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: var(--font-family);
+  }
+
+  body {
+    background-color: rgba(0, 255, 102, 0.2);
+    min-width: 360px;
+    height: 100%;
+    width: 100%;
+  }
+
+  button {
+    font-weight: 700;
+    font-size: 18px;
+    height: 48px;
+    width: 232px;
+    border-radius: 8px;
+    box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.3);
+  }
+`;
